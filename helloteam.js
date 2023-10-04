533fa1512184c1a701fa96a6ebdc1a6c2f98a99d
lg10migue <lop18028@byui.edu>
2023-10-03 18:29:27 -0600

    Update loop for helloteam script.
    Added my name to the script and refactor for loop.

--- helloteam.js
+++ helloteam.js
@@ -4,9 +4,6 @@
 //3 October 2023
 //Hello Team Program
 
-const teamNames = ["Michael", "Cigi", "Tadeo"];
+const teamNames = ["Michael", "Cigi", "Tadeo", "Miguel"];
 
-for(let i = 0; i < teamNames.length; i++)
-{
-    console.log("Hello " + teamNames[i]);
-}
+teamNames.forEach( ( name ) => console.log( `Hello ${name}!` ) );
